2deb2d93b1d23a0b0bd4e3b226f73352012ffd91
Mouad Lasri <mouadkor20@gmail.com>
2025-08-30 02:12:16 +0900

    Initial project setup

---
+++ src/main/java/org/financetracker/apifinancetracker/ApiFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.financetracker.apifinancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinanceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFinanceTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/financetracker/apifinancetracker/ApiFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.financetracker.apifinancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinanceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
